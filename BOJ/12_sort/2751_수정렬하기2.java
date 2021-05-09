958a71d2f9d51d5528714c53d42c23a0ce320a93
heeejoo0518 <42292066+heeejoo0518@users.noreply.github.com>
2021-05-10 02:32:31 +0900

    Create 2751_수정렬하기2.java

---
+++ BOJ/12_sort/2751_수정렬하기2.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		int n = in.nextInt();
+		
+		ArrayList<Integer> num = new ArrayList<>();
+		
+		for(int i=0;i<n;i++) {
+			num.add(in.nextInt());
+		}
+		
+		in.close();
+		
+		Collections.sort(num);
+		
+		StringBuilder sb = new StringBuilder();
+		
+		for(int i=0;i<n;i++) {			
+			sb.append(num.get(i)+"\n");
+		}
+		
+		System.out.println(sb.toString());
+	}
+}

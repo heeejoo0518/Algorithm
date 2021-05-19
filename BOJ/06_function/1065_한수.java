58106bc24e2fdf867cbf6a06daab6a9a9246145b
heeejoo0518 <42292066+heeejoo0518@users.noreply.github.com>
2021-05-19 23:52:16 +0900

    Update 1065_한수.java
    1~99는 모두 한수, 1000은 한수가 아님
    따라서 세자리수만 체크
    반복문은 최대 n-99번 O(n)
    함수형식으로 변경

--- BOJ/06_function/1065_한수.java
+++ BOJ/06_function/1065_한수.java
@@ -8,10 +8,13 @@ public class Main{
 		int n = Integer.parseInt(br.readLine());		
 		br.close();
 		
+		System.out.println(cntN(n));
+	}
+	
+	public static int cntN(int n) {
 		if(n<=99) {
 			//99이하인 수는 모두 한수
-			System.out.println(n);
-			return;
+			return n;
 		}
 		
 		int cnt = 99;
@@ -19,6 +22,7 @@ public class Main{
 			if(i==1000) break; //n 최대숫자 1000은 한수가 아님
 			if(i%10-(i/10)%10 == (i/10)%10-(i/100)%10) cnt++;
 		}
-		System.out.println(cnt);
+
+		return cnt;
 	}
 }

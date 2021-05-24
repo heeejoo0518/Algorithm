dcb3c5f7dd2c5ba355555f9838d39938cfe31623
heeejoo0518 <42292066+heeejoo0518@users.noreply.github.com>
2021-05-24 09:32:46 +0900

    Update 가장 큰 수.java
    수가 너무 커서 long으로도 안담기는 경우가 있음
    따라서 맨 앞글자가 '0'인 경우를 찾아서 따로 "0"리턴

--- Programmers/가장 큰 수.java
+++ Programmers/가장 큰 수.java
@@ -2,24 +2,27 @@ import java.util.*;
 class Solution {
     public String solution(int[] numbers) {
         String[] str = new String[numbers.length];
-		
-		for(int i=0;i<numbers.length;i++) {
-			str[i] = String.valueOf(numbers[i]);
-		}
-        
-		Arrays.sort(str,new Comparator<String>() {
-			@Override
-			public int compare(String o1, String o2) {
-				return Integer.parseInt(o2+o1) - Integer.parseInt(o1+o2);
-			}
-		});
 
-		StringBuilder sb = new StringBuilder();
-		for(String s:str) {
-			sb.append(s);
-		}
+        for(int i=0;i<numbers.length;i++) {
+            str[i] = String.valueOf(numbers[i]);
+        }
 
-        return Long.valueOf(sb.toString()).toString();
-        //return sb.toString();
+        Arrays.sort(str,new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return Integer.parseInt(o2+o1) - Integer.parseInt(o1+o2);
+            }
+        });
+
+        StringBuilder sb = new StringBuilder();
+        boolean zero = true;
+
+        for(String s:str) {
+            sb.append(s);
+            if(!s.equals("0")) zero = false;
+        }
+
+        if(zero) return "0";
+        else return sb.toString(); 
     }
 }

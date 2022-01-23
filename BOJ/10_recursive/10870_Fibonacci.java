7e96961aec2fef4ddd3ee093ba904b272fc7aea5
heeejoo0518 <42292066+heeejoo0518@users.noreply.github.com>
2022-01-23 19:02:32 +0900

    Update 10870_Fibonacci.java
    로직 조금 수정-배열사용

--- BOJ/10_recursive/10870_Fibonacci.java
+++ BOJ/10_recursive/10870_Fibonacci.java
@@ -1,18 +1,23 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		int n = in.nextInt();
+	static int[] fibonacci = new int[21];
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(in.readLine());
+		
 		in.close();
 		
-		System.out.println(Fibonacci(n));
-		
-		
+		System.out.println(getFibonacci(n));
 	}
-	public static int Fibonacci(int n) {
-		if(n<=0) return 0;
-		if(n==1) return 1;
-		return Fibonacci(n-1)+Fibonacci(n-2);
+
+	public static int getFibonacci(int n) {
+		if(n<=1) return n;	//이러면 fibonacci[1]에 저장된 값은 항상 0임. 
+							//피보나치 수를 전부 출력하는 게 아니므로 신경x
+		if(fibonacci[n]!=0) return fibonacci[n];
+		return fibonacci[n] = getFibonacci(n-1)+getFibonacci(n-2);
 	}
 }
+

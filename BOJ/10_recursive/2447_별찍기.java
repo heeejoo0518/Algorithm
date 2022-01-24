116f360e060f42e45c456bc8fdb229c0b61799bf
heeejoo0518 <42292066+heeejoo0518@users.noreply.github.com>
2022-01-24 22:05:23 +0900

    Update 2447_별찍기.java
    스페이스 따로 안찍고 프린트할때 체크

--- BOJ/10_recursive/2447_별찍기.java
+++ BOJ/10_recursive/2447_별찍기.java
@@ -1,62 +1,61 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Arrays;
 
-public class Main{
+public class BOJ_2447_별찍기10 {
 	static char[][] board;
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));		
-		int n = Integer.parseInt(br.readLine());		
-		br.close();
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(in.readLine());
 		
-		board = new char[n][n];
+		in.close();
 		
-		star(0,0,n);
-		
+		board = new char[N][N];
+
+		printStar(0, 0, N);
+
+		printBoard();
+
+	}
+
+	private static void printStar(int i, int j, int n) {
+		if (n == 1) {
+			board[i][j] = '*';
+			return;
+		}
+
+		int nextN = n / 3;
+
+		for (int k = 0; k < 3; k++) {
+			for (int l = 0; l < 3; l++) {
+				if (k == 1 && l == 1) continue;		//비어야 하는 부분 continue;
+				printStar(i + nextN * k, j + nextN * l, nextN);
+			}
+		}
+	}
+
+	private static void printBoard() {
 		StringBuilder sb = new StringBuilder();
-		
-		for(int i=0;i<n;i++) {
-			for(int j=0;j<n;j++) {
-				sb.append(board[i][j]);
+		for (int i = 0; i < board.length; i++) {
+			for (int j = 0; j < board.length; j++) {
+				if (board[i][j] == '*')
+					sb.append(board[i][j]);
+				else
+					sb.append(' '); // 공백은 따로 체크
 			}
 			sb.append("\n");
 		}
-		
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		bw.write(sb.toString());
-		
-		bw.flush();
-		bw.close();
-	}
-	
-	public static void star(int x, int y, int n) {
-		if(n==1) {
-			board[y][x] = '*';
-			return;
-		}		
-		
-		int div = n/3;
-		
-		for(int i=0;i<3;i++) {
-			for(int j=0;j<3;j++) {
-				if(i==1 && j==1) {
-					space(x+j*div, y+i*div, div);
-					continue;
-				}
-				star(x+j*div, y+i*div, div);
-			}
-		}
-		
-	}
-	
-	public static void space(int x,int y, int n) {
-		for(int i=y;i<y+n;i++) {
-			for(int j=x;j<x+n;j++) {
-				board[i][j] = ' ';
-			}
-		}
+		System.out.println(sb.toString());
 	}
 }
+
+
+// printStar 변수 넘길 때 간단하게 하려고 짜봤다가 머리만 터짐^^...
+//for (int ni = i; ni < i + n; ni += nextN) {
+//	for (int nj = j; nj < j + n; nj += nextN) {
+//		if (ni == i + nextN && nj == j + nextN)
+//			continue;
+//		printStar(ni, nj, nextN);
+//	}
+//}

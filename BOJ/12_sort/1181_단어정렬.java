606b286c92cf7a6eeafb7ec220967c7d2faca112
heeejoo0518 <42292066+heeejoo0518@users.noreply.github.com>
2022-01-30 22:18:39 +0900

    Update 1181_단어정렬.java
    set, lambda 사용

--- BOJ/12_sort/1181_단어정렬.java
+++ BOJ/12_sort/1181_단어정렬.java
@@ -3,37 +3,47 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Arrays;
-import java.util.Comparator;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
-public class Main{
+public class BOJ_1181_단어정렬 {
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));		
-		int n = Integer.parseInt(br.readLine());
-		String[] words = new String[n];
-		for(int i=0;i<n;i++) {
-			words[i] = br.readLine();
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(in.readLine());
+		
+		Set<String> set = new HashSet<>();
+		
+		//입력
+		for(int i=0;i<N;i++) {
+			set.add(in.readLine());			
 		}
+		in.close();
 		
-		br.close();
+		//정렬
+		List<String> words = new ArrayList<>(set);
+		Collections.sort(words, ((s1,s2)->{
+			if(s1.length()==s2.length()) return s1.compareTo(s2);
+			return s1.length()-s2.length();
+		}));
 		
-		Arrays.sort(words,new Comparator<String>(){
-			@Override
-			public int compare(String s1, String s2) {
-				if(s1.length()!=s2.length()) return s1.length()-s2.length();
-				else return s1.compareTo(s2);
-			}
-		});
-		
-		StringBuilder sb = new StringBuilder();		
-		for(String s:Arrays.stream(words).distinct().toArray(String[]::new)) {
-			sb.append(s+"\n");
+		//출력
+		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+		for(String word:words) {
+			out.write(word+"\n");
 		}
-		
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		bw.write(sb.toString());
-		
-		bw.flush();
-		bw.close();
+		out.flush();
+		out.close();
 	}
 }
+
+/*
+  중복 삭제하는 다른 방법(Stream)
+ 
+	for(String s:Arrays.stream(words).distinct().toArray(String[]::new)) {
+		sb.append(s+"\n");
+	}
+
+*/

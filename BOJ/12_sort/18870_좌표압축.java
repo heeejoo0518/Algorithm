f7af8160e2519f38d4eb37a358ea26c5b0d6a041
heeejoo0518 <42292066+heeejoo0518@users.noreply.github.com>
2022-01-31 12:22:45 +0900

    Update 18870_좌표압축.java
    지금 이 방법은 객체리스트를 정렬하고 정렬된 리스트의 idx값을 이용해 result배열에 rank넣는것
    
    중복제거한 list에서 처음 입력받은 숫자들 binarysearch할 수도 있음! (인덱스값 리턴됨=rank)
    Arrays.stream(처음입력받은숫자들배열).distinct().sorted().collect(Collectors.toList());

--- BOJ/12_sort/18870_좌표압축.java
+++ BOJ/12_sort/18870_좌표압축.java
@@ -1,39 +1,59 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.StringTokenizer;
-import java.util.stream.Collectors;
 
-public class Main{
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));		
+class Location implements Comparable<Location>{
+	int idx;
+	int x;
+	
+	public Location(int idx,int x) {
+		this.idx = idx;
+		this.x = x;
+	}
+	
+	@Override
+	public int compareTo(Location l) {
+		return this.x-l.x;
+	}
+	
+}
+
+public class BOJ_18870_좌표압축 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(in.readLine());
 		
-		int n = Integer.parseInt(br.readLine());
-		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		List<Location> locations = new ArrayList<>();
 		
-		br.close();
+		//입력
+		StringTokenizer st = new StringTokenizer(in.readLine());
+		for(int i=0;i<N;i++) {
+			locations.add(new Location(i,Integer.parseInt(st.nextToken())));
+		}
+		in.close();
 		
-		Integer[] nums = new Integer[n];
-		for(int i=0;st.hasMoreTokens();i++) {
-			nums[i] = Integer.parseInt(st.nextToken());
+		//정렬
+		Collections.sort(locations);
+		
+		//순서 저장
+		int rank = 0;
+		int[] result = new int[N];
+		for(int i=0;i<N;i++) {
+			if(i!=0 && locations.get(i-1).x != locations.get(i).x) rank++;
+			result[locations.get(i).idx] = rank;
 		}
 		
-		List<Integer> num_list = Arrays.stream(nums).distinct().sorted().collect(Collectors.toList());
-		
-		StringBuilder sb = new StringBuilder();		
-		for(int i=0;i<n;i++) {
-			sb.append(Collections.binarySearch(num_list, nums[i])+" ");
+		//출력
+		StringBuilder sb = new StringBuilder();
+		for(int res:result) {
+			sb.append(res).append(" ");
 		}
 		
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		bw.write(sb.toString());
+		System.out.println(sb.toString());
 		
-		bw.flush();
-		bw.close();
 	}
 }

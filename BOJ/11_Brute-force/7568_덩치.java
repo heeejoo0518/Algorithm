2af411e191d24472febb890b15f4a1aa3b4ceda8
heeejoo0518 <42292066+heeejoo0518@users.noreply.github.com>
2022-02-01 19:33:27 +0900

    Update 7568_덩치.java

--- BOJ/11_Brute-force/7568_덩치.java
+++ BOJ/11_Brute-force/7568_덩치.java
@@ -1,72 +1,54 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
+import java.util.StringTokenizer;
 
-class Body implements Comparable<Body>{
-	int weight;
-	int height;
-	
-	public Body(int weight, int height) {
-		this.weight = weight;
-		this.height = height;
-	}	
-	
-	@Override
-	public int compareTo(Body o) {
-		if(this.height > o.height && this.weight > o.weight) 
-			return 1;
-		else if(this.height < o.height && this.weight < o.weight) 
-			return -1;
-		else 
-			return 0;
-	}
+public class BOJ_7568_덩치 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(in.readLine());
+        List<Body> persons = new ArrayList<>();
+
+        //입력
+        StringTokenizer st;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(in.readLine());
+            persons.add(new Body(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
+        }
+        in.close();
+
+        //탐색
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            int tmp = 1;
+            for (int j = 0; j < N; j++) {
+                if(j==i) continue;
+                if (persons.get(j).compareTo(persons.get(i)) > 0) tmp++;
+            }
+            sb.append(tmp).append(" ");
+        }
+
+        //출력
+        System.out.println(sb.toString());
+    }
 
 }
 
+class Body implements Comparable<Body> {
+    int height;
+    int weight;
 
-public class Main{	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		//값 입력받기
-		int n = Integer.parseInt(br.readLine());
-		
-		List<Body> list = new ArrayList<>();
-		for(int i=0;i<n;i++) {
-			String[] tmpXY = br.readLine().split(" ");
-			list.add(new Body(Integer.parseInt(tmpXY[0]),Integer.parseInt(tmpXY[1])));
-		}
-		
-		br.close();
-		
-		//등수 구하기
-		int[] rank = new int[n];
-		Arrays.fill(rank, 1);
-		
-		for(int i=0;i<n;i++) {
-			for(int j=0;j<n;j++) {
-				if(list.get(i).compareTo(list.get(j))<0) rank[i]++;  
-			}
-		}
-		
-		//출력
-		StringBuilder sb = new StringBuilder();
-		
-		for(int i=0;i<n;i++) {
-			sb.append(rank[i]+" ");
-		}
-		
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-		bw.write(sb.toString());		
-		
-		bw.flush();
-		bw.close();
-	}
+    public Body(int weight, int height) {
+        this.weight = weight;
+        this.height = height;
+    }
+
+    @Override
+    public int compareTo(Body o) {
+        if (this.height > o.height && this.weight > o.weight) return 1;
+        else if (this.height < o.height && this.weight < o.weight) return -1;
+        return 0;
+    }
 }

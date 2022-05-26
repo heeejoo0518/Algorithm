5ac3d988385e64311aba0ac118aab5a2c176c760
heeejoo0518 <42292066+heeejoo0518@users.noreply.github.com>
2022-05-27 00:05:21 +0900

    Create BOJ_11654_아스키코드.java

---
+++ BOJ/5월/BOJ_11654_아스키코드.java
@@ -0,0 +1,8 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		int A = new BufferedReader(new InputStreamReader(System.in)).readLine().charAt(0);
+		System.out.println(A);
+	}
+}

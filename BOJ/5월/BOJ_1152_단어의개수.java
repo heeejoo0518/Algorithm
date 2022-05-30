880ae86c0d7776295c88843034c8f05fe7dda535
heeejoo0518 <42292066+heeejoo0518@users.noreply.github.com>
2022-05-30 09:40:51 +0900

    Create BOJ_1152_단어의개수.java

---
+++ BOJ/5월/BOJ_1152_단어의개수.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.*;
+
+public class BOJ_1152_단어의개수 {
+	public static void main(String[] args) throws IOException {
+		StringTokenizer st = new StringTokenizer(new BufferedReader(new InputStreamReader(System.in)).readLine());
+		System.out.println(st.countTokens());
+	}
+}

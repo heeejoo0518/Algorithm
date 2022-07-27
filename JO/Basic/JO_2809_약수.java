d34477c2f737f5d41835df72b9036630d887fcf4
heeejoo0518 <42292066+heeejoo0518@users.noreply.github.com>
2022-07-28 00:22:09 +0900

    Update JO_2809_약수.java

--- JO/Basic/JO_2809_약수.java
+++ JO/Basic/JO_2809_약수.java
@@ -1,14 +1,24 @@
 import java.io.*;
+import java.util.*;
 
 public class JO_2809_약수 {
 	public static void main(String[] args) throws IOException {
 		int N = Integer.parseInt(new BufferedReader(new InputStreamReader(System.in)).readLine());
-		StringBuilder sb = new StringBuilder();
-		for(int i=1;i<=N;i++){
-			if(N%i==0) sb.append(i).append(' ');
+		StringBuilder front = new StringBuilder();
+		ArrayList<Integer> back = new ArrayList<>();
+		double sqrt = Math.sqrt(N);
+		for(int i=1;i<=sqrt;i++){
+			if(N%i==0) {
+				front.append(i).append(' ');
+				if(i!=sqrt) back.add(N/i);
+			}
 		}
 
-		System.out.println(sb);
+		for(int i=back.size()-1;i>=0;i--){
+			front.append(back.get(i)).append(' ');
+		}
+
+		System.out.println(front);
 	}
 
 }

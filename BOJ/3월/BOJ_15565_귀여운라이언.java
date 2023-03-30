f2ef8e160f17a4ac1343f3def8565b547bc8d8d5
heeejoo0518 <heeejoo0518@gmail.com>
2023-03-31 00:40:52 +0900

    boj 15565 귀여운 라이언

--- BOJ/3월/BOJ_15565_귀여운라이언.java
+++ BOJ/3월/BOJ_15565_귀여운라이언.java
@@ -11,14 +11,14 @@ public class BOJ_15565_귀여운라이언 {
 		st = new StringTokenizer(in.readLine());
 		for (int i = 0; i < N; i++) toy[i] = st.nextToken().equals("1");
 
-		int min = N + 1, lian = toy[0] ? 1 : 0;
+		int min = N + 1, ryan = toy[0] ? 1 : 0;
 		int l = 0, r = 1;
 		while (l < r) {
-			if (lian >= K || r >= N) {
-				if(lian >= K) min = Math.min(min, r - l);
-				if (toy[l++]) --lian;
+			if (ryan >= K || r >= N) {
+				if (ryan >= K) min = Math.min(min, r - l);
+				if (toy[l++]) --ryan;
 			} else if (toy[r++]) {
-				++lian;
+				++ryan;
 			}
 		}
 

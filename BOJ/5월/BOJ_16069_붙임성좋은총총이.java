e3771bf757455309283fca423e306e5d3552ff1a
heeejoo0518 <42292066+heeejoo0518@users.noreply.github.com>
2023-05-04 23:36:04 +0900

    boj 16069 붙임성 좋은 총총이

---
+++ BOJ/5월/BOJ_16069_붙임성좋은총총이.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+ 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(in.readLine());
+        Set<String> set = new HashSet<>();
+        set.add("ChongChong");
+ 
+        StringTokenizer st;
+        for(int i=0;i<N;i++){
+            st = new StringTokenizer(in.readLine());
+            String a = st.nextToken();
+            String b = st.nextToken();
+            if(set.contains(a) || set.contains(b)){
+                set.add(a);
+                set.add(b);
+            }
+        }
+ 
+        System.out.println(set.size());
+    }
+}

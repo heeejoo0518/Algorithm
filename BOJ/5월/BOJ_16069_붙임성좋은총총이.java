9faecea653d1bf15d46799e84548681f9456ae77
heeejoo0518 <heeejoo0518@gmail.com>
2023-05-05 15:24:20 +0900

    update 16069 클래스이름변경

--- BOJ/5월/BOJ_16069_붙임성좋은총총이.java
+++ BOJ/5월/BOJ_16069_붙임성좋은총총이.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
  
-public class Main {
+public class BOJ_16069_붙임성좋은총총이 {
     public static void main(String[] args) throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(in.readLine());

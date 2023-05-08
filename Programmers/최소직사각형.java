925668422baa345d53ab4c1d48b6a18eee00f0fe
heeejoo0518 <42292066+heeejoo0518@users.noreply.github.com>
2023-05-08 23:26:50 +0900

    programmers 최소직사각형

---
+++ Programmers/최소직사각형.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] sizes) {
+        
+        int width=0, height=0;
+        for(int[] size:sizes){
+            width = Math.max(width, Math.max(size[0], size[1]));
+            height = Math.max(height, Math.min(size[0], size[1]));
+        }
+        
+        return width*height;
+    }
+}

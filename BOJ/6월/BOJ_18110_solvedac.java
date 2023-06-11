029256c408ba5ff253a52143ea6fa910487388d5
heeejoo0518 <42292066+heeejoo0518@users.noreply.github.com>
2023-06-11 21:16:05 +0900

    create :b: 18110 solved.ac

---
+++ BOJ/6월/BOJ_18110_solvedac.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(in.readLine());
+		int[] score = new int[N];
+		for(int i=0;i<N;i++) score[i] = Integer.parseInt(in.readLine());
+		Arrays.sort(score);
+		int out = (int)Math.round(N*0.15);
+
+		int sum = 0;
+		for(int i=out;i<N-out;i++) sum += score[i];
+		int avg = (int)Math.round((double)sum/(N-2*out));
+		System.out.println(avg);
+	}
+}

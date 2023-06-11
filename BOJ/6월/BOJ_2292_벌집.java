8ce2246cafedec67133b92fba495671cd81b9b1a
heeejoo0518 <42292066+heeejoo0518@users.noreply.github.com>
2023-06-11 20:49:06 +0900

    create :b: 2292 벌집

---
+++ BOJ/6월/BOJ_2292_벌집.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(in.readLine());
+		int X = (int)Math.round(Math.pow((N-1)/(double)3,0.5));
+		System.out.println(X+1);
+
+		//a(n) = 3n^2 - 3n + 1
+		//		x = ((n-1)/3)**(1/2)
+		//print(round(x)+1)
+	}
+}

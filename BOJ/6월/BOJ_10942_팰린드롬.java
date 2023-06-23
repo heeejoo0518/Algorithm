b8af69a661559bbf5cdb5aae56796b530805f2b3
heeejoo0518 <heeejoo0518@gmail.com>
2023-06-24 01:53:39 +0900

    update :b: 10942 팰린드롬 인덱스수정

--- BOJ/6월/BOJ_10942_팰린드롬.java
+++ BOJ/6월/BOJ_10942_팰린드롬.java
@@ -5,20 +5,21 @@ public class BOJ_10942_팰린드롬 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(in.readLine());
-		int[] arr = new int[N+1];
+		int[] arr = new int[N];
 		StringTokenizer st = new StringTokenizer(in.readLine());
-		for(int i=1;i<=N;i++) arr[i] = Integer.parseInt(st.nextToken());
+		for(int i=0;i<N;i++) arr[i] = Integer.parseInt(st.nextToken());
 
-		boolean[][] dp = new boolean[N+1][N+1];
-		for(int i=1;i<=N;i++){
+		boolean[][] dp = new boolean[N][N];
+		for(int i=0;i<N;i++){
 			dp[i][i] = true;
-			if(i+1<=N && arr[i]== arr[i+1]) dp[i][i+1] = true;
-
+			if(i+1<N && arr[i]== arr[i+1]) dp[i][i+1] = true;
 		}
 
-		for(int i=2;i<N;i++){
-			for(int j=1;j<=N-i;j++){
-				if(arr[j]==arr[j+i] && dp[j+1][j+i-1]) dp[j][j+i] = true;
+		for(int len=2;len<N;len++){
+			for(int start=0;start<N-len;start++){
+				//시작과 끝점(start+len)이 같고 dp[시작 다음점][끝 이전 점]이 팰린드롬이라면
+				//dp[start][start+len]도 팰린드롬
+				if(arr[start]==arr[start+len] && dp[start+1][start+len-1]) dp[start][start+len] = true;
 			}
 		}
 
@@ -26,7 +27,7 @@ public class BOJ_10942_팰린드롬 {
 		StringBuilder sb = new StringBuilder();
 		for(int i=0;i<M;i++){
 			st = new StringTokenizer(in.readLine());
-			if(dp[Integer.parseInt(st.nextToken())][Integer.parseInt(st.nextToken())]) sb.append("1\n");
+			if(dp[Integer.parseInt(st.nextToken())-1][Integer.parseInt(st.nextToken())-1]) sb.append("1\n");
 			else sb.append("0\n");
 		}
 		System.out.print(sb);
